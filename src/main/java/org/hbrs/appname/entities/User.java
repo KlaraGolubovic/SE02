1b99654f60bfbe1323b1e03690518a22ba004534
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2022-04-14 15:03:13 +0200

    refactor: cleanup code of user entity

--- src/main/java/org/hbrs/appname/entities/User.java
+++ src/main/java/org/hbrs/appname/entities/User.java
@@ -1,147 +1,91 @@
 package org.hbrs.appname.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
-// import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "user", schema = "carlook")
 public class User {
-    private int id;
-    private LocalDate dateOfBirth;
-    private String email;
-    private String firstName;
-    private String lastName;
-    private String occupation;
-    private String password;
-    private String phone;
-    private String userid;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private int id = -1;
+
+    @Basic
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth = LocalDate.now();
+
+    @Basic
+    @Column(name = "email", unique = true, nullable = false)
+    private String email = "";
+
+    @Basic
+    @Column(name = "first_name", nullable = false)
+    private String firstName = "";
+
+    @Basic
+    @Column(name = "last_name", nullable = false)
+    private String lastName = "";
+
+    @Basic
+    @Column(name = "occupation", nullable = false)
+    private String occupation = "";
+
+    @Basic
+    @Column(name = "password", nullable = false)
+    private String password = "";
+
+    @Basic
+    @Column(name = "phone", nullable = false)
+    private String phone = "";
+
+    @Basic
+    @Column(name = "userid", nullable = false)
+    private String userid = "";
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_to_rolle",
+            catalog = "demouser",
+            schema = "carlook",
+            joinColumns = @JoinColumn(
+                    name = "userid",
+                    referencedColumnName = "id",
+                    nullable = false
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "bezeichnung",
+                    referencedColumnName = "bezeichhnung",
+                    nullable = false
+            )
+    )
     private List<Rolle> roles;
 
-    @Id
-    @GeneratedValue
-    @Column(name = "id")
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Basic
-    @Column(name = "date_of_birth")
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    @Basic
-    @Column(name = "email")
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Basic
-    @Column(name = "first_name")
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @Basic
-    @Column(name = "last_name")
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Basic
-    @Column(name = "occupation")
-    public String getOccupation() {
-        return occupation;
-    }
-
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
-
-    @Basic
-    @Column(name = "password")
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Basic
-    @Column(name = "phone")
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    @Basic
-    @Column(name = "userid")
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
     @Override
-    public boolean equals(Object o) {
-        if (this == o)
+    public boolean equals(Object obj) {
+        if (this == obj) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (obj == null || getClass() != obj.getClass()) {
             return false;
-        User user = (User) o;
-        return id == user.id &&
-                Objects.equals(dateOfBirth, user.dateOfBirth) &&
-                Objects.equals(email, user.email) &&
-                Objects.equals(firstName, user.firstName) &&
-                Objects.equals(lastName, user.lastName) &&
-                Objects.equals(occupation, user.occupation) &&
-                Objects.equals(password, user.password) &&
-                Objects.equals(phone, user.phone) &&
-                Objects.equals(userid, user.userid);
+        }
+        User user = (User) obj;
+        return id == user.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, dateOfBirth, email, firstName, lastName, occupation, password, phone, userid);
+        return Objects.hash(id);
     }
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_to_rolle", catalog = "demouser", schema = "carlook", joinColumns = @JoinColumn(name = "userid", referencedColumnName = "id", nullable = false), inverseJoinColumns = @JoinColumn(name = "bezeichnung", referencedColumnName = "bezeichhnung", nullable = false))
-    public List<Rolle> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<Rolle> roles) {
-        this.roles = roles;
-    }
-
 }
