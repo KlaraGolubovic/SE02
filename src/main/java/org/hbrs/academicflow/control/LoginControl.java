d27f1a0dc21dbb212270e2b5dfcf7c8cbea01b0e
Gökhan Topcu <64787457+JailBreakerTV@users.noreply.github.com>
2022-04-16 21:23:55 +0200

    feat: add function for listing all existing users fix: remove unused classes

--- src/main/java/org/hbrs/academicflow/control/LoginControl.java
+++ src/main/java/org/hbrs/academicflow/control/LoginControl.java
@@ -3,8 +3,11 @@ package org.hbrs.academicflow.control;
 import org.hbrs.academicflow.control.exception.DatabaseUserException;
 import org.hbrs.academicflow.model.user.dto.UserDTO;
 import org.hbrs.academicflow.model.user.UserService;
+import org.hbrs.academicflow.util.Encryption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.security.NoSuchAlgorithmException;
 
 @Component
 public class LoginControl {
@@ -15,7 +18,12 @@ public class LoginControl {
 
     public boolean authentificate(String username, String password) throws DatabaseUserException {
         // Standard: User wird mit Spring JPA ausgelesen (Was sind die Vorteile?)
-        UserDTO tmpUser = this.getUserWithJPA(username, password);
+        UserDTO tmpUser = null;
+        try {
+            tmpUser = this.getUserWithJPA(username, password);
+        } catch (NoSuchAlgorithmException e) {
+            return false;
+        }
 
         // Alternative: Auslesen des Users mit JDBC (Was sind die Vorteile bzw.
         // Nachteile?)
@@ -34,10 +42,11 @@ public class LoginControl {
 
     }
 
-    private UserDTO getUserWithJPA(String username, String password) throws DatabaseUserException {
+    private UserDTO getUserWithJPA(String username, String password) throws DatabaseUserException, NoSuchAlgorithmException {
         UserDTO userTmp;
         try {
-            userTmp = this.service.findUserByIdAndPassword(username, password);
+            //userTmp = this.service.findUserByIdAndPassword(username, password);
+            userTmp = this.service.findUserByIdAndPassword(username, Encryption.sha256(password));
         } catch (org.springframework.dao.DataAccessResourceFailureException e) {
             throw new DatabaseUserException("A failure occurred while trying to connect to database with JPA");
         }
