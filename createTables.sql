ade599ce6f629eaf35b51b813cc3a458b2187e41
Tom Keppeler <49951477+TomKeppeler@users.noreply.github.com>
2022-05-03 21:12:44 +0200

    update: TableCreate & erd

--- createTables.sql
+++ createTables.sql
@@ -1,55 +1,86 @@
-DROP TABLE backend.USER CASCADE CONSTRAINT;
-DROP TABLE backend.PERMISSION_GROUP CASCADE CONSTRAINT;
-DROP TABLE backend.USER_GROUP CASCADE CONSTRAINT;
-DROP TABLE backend.PROFILE CASCADE CONSTRAINT;
-DROP TABLE backend.JOB_AD CASCADE CONSTRAINT;
-CREATE TABLE backend.USER (
+DROP TABLE public.USER CASCADE;
+DROP TABLE public.PERMISSION_GROUP CASCADE;
+DROP TABLE public.USER_GROUP CASCADE;
+DROP TABLE public.PROFILE CASCADE;
+DROP TABLE public.JOB_AD CASCADE;
+drop table public.comp_user CASCADE;
+drop table public.stud_user CASCADE;
+drop table public.comp_profile CASCADE;
+drop table public.stud_profile CASCADE;
+
+CREATE TABLE public.USER (
     user_id int NOT NULL,
-    date_of_birth date NOT NULL,
 	email varchar NOT NULL,
-	first_name varchar NOT NULL,
-	last_name varchar NOT NULL,
-	occupation varchar,
 	password varchar NOT NULL,
-	phone varchar,
 	username varchar NOT null,
 	CONSTRAINT user_user_id_pk PRIMARY KEY(user_id),
-	CONSTRAINT user_email_qk UNIQUE KEY(email),
+	CONSTRAINT user_email_qk UNIQUE(email)
 );
 
-CREATE TABLE backend.PERMISSION_GROUP(
+CREATE TABLE public.PERMISSION_GROUP(
 	group_name varchar NOT NULL,
 	level int NOT NULL DEFAULT -1,
 	constraint permission_group_pk PRIMARY KEY(group_name)
 );
 
-CREATE TABLE backend.USER_GROUP(
+CREATE TABLE public.USER_GROUP(
 	user_id int NOT NULL,
 	group_name varchar DEFAULT 'GroupName' NOT NULL,
 	CONSTRAINT user_group_pk PRIMARY KEY(user_id, group_name),
-	CONSTRAINT user_group_user_id_fk FOREIGN KEY (user_id) REFERENCES backend.USER(user_id),
-	CONSTRAINT user_group_group_name_fk FOREIGN KEY (group_name) REFERENCES backend.group_name(group_name),
+	CONSTRAINT user_group_user_id_fk FOREIGN KEY (user_id) REFERENCES public.USER(user_id),
+	CONSTRAINT user_group_group_name_fk FOREIGN KEY (group_name) REFERENCES public.PERMISSION_GROUP(group_name)
 );
 
-CREATE TABLE backend.profile (
-	profile_id int NOT NULL,
-	user_id int NOT NULL,
+CREATE TABLE public.comp_user (
+	comp_user_id int NOT NULL,
+	user_id int not null,
+	name varchar not null,
+	phone varchar,
+	CONSTRAINT comp_user_pk PRIMARY key(comp_user_id),
+	CONSTRAINT comp_user_fk FOREIGN key(user_id) REFERENCES public.user(user_id)
+);
+
+CREATE TABLE public.stud_user (
+	stud_user_id int NOT NULL,
+	user_id int not null,
+	date_of_birth date not null,
+	first_name varchar not null,
+	last_name varchar not null,
+	phone varchar,
+	CONSTRAINT stud_user_pk PRIMARY key(stud_user_id),
+	CONSTRAINT stud_user_fk FOREIGN key(user_id) REFERENCES public.user(user_id)
+);
+
+
+CREATE TABLE public.comp_profile (
+	comp_profile_id int NOT NULL,
+	comp_user_id int NOT NULL,
 	description varchar NULL,
 	address varchar NOT NULL,
-	docs bytea NULL,
-	CONSTRAINT profile_pk PRIMARY KEY (profile_id),
-	CONSTRAINT profile_fk FOREIGN KEY (user_id) REFERENCES backend."user"(user_id)
+	docs bytea,
+	CONSTRAINT comp_profile_pk PRIMARY KEY (comp_profile_id),
+	CONSTRAINT comp_profile_fk FOREIGN KEY (comp_user_id) REFERENCES public.comp_user(comp_user_id)
 );
 
-CREATE TABLE backend.JOB_AD(
+CREATE TABLE public.stud_profile (
+	stud_profile_id int NOT NULL,
+	stud_user_id int NOT NULL,
+	description varchar NULL,
+	address varchar NOT NULL,
+	docs bytea,
+	CONSTRAINT stud_profile_pk PRIMARY KEY (stud_profile_id),
+	CONSTRAINT stud_profile_fk FOREIGN KEY (stud_user_id) REFERENCES public.stud_user(stud_user_id)
+);
+
+CREATE TABLE public.JOB_AD(
 	job_ad_id int NOT NULL,
-	profile_id int NOT null,
+	comp_profile_id int NOT null,
 	title varchar NOT NULL,
-	descrbtion varchar,
+	description varchar,
 	deadline date,
 	job_typ varchar NOT NULL,
 	location varchar NOT NULL,
 	remote boolean NOT null,
 	CONSTRAINT job_ad_id_pk PRIMARY KEY (job_ad_id),
-	CONSTRAINT job_ad_fk FOREIGN key (profile_id) REFERENCES backend.PROFILE(profile_id)
+	CONSTRAINT comp_job_ad_fk FOREIGN key (comp_profile_id) REFERENCES public.comp_profile(comp_profile_id)
 );
